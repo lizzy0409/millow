d45b665cf7b8dfa2334c2ada44b7511ed715ad8d
helios <lizzy040925@gmail.com>
2023-06-08 19:47:09 +0500

    Updated Navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -18,7 +18,7 @@ const Navigation = ({ account, setAccount }) => {
 
             <div className='nav__brand'>
                 <img src={logo} alt="Logo" />
-                <h1>Millow</h1>
+                <h1>Real Token</h1>
             </div>
 
             {account ? (
